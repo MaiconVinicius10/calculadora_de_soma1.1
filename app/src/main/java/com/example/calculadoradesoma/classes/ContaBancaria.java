c9a63c57b28607f02a8ffea35ad485a7ca7332cc
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-11-11 16:09:01 -0300

    Criando classe para aprender usar os modificadores acesso.

---
+++ app/src/main/java/com/example/calculadoradesoma/classes/ContaBancaria.java
@@ -0,0 +1,10 @@
+package com.example.calculadoradesoma.classes;
+// modificador de acesso default só consigo utlizar dentro da mesma classe e pacote
+class ContaBancaria {
+    double saldo = 0;
+
+    void sacar(){
+    }
+    void tranferir(){}
+
+}
